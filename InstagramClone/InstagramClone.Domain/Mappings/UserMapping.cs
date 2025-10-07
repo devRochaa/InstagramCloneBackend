0deaa941d527cc9e34691cca94850e5623437e20
marco.desantis <marco.desantis@onfriday.com.br>
2025-10-07 16:36:55 -0300

    feat: UserMapping

---
+++ InstagramClone/InstagramClone.Domain/Mappings/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿namespace InstagramClone.Domain.Mappings;
+
+internal static class UserMapping
+{
+    //public static UserModel ToUserModel(this PersonEntity entity) => new()
+    //{
+    //    Id = entity.Id,
+    //    CreatedAt = entity.CreatedAt,
+    //    LastUpdatedAt = entity.LastUpdatedAt,
+    //    FullName = entity.FullName,
+    //    Email = entity.Email,
+    //    Phone = entity.Phone,
+    //    NationalDocument = entity.NationalDocument,
+    //};
+}

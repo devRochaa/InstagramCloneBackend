f09a7047d8649103f79d24811031dcdeafb883f8
devRochaa <danielrocha.etec@gmail.com>
2025-10-07 22:55:59 -0300

    start implementing repositories

--- src/InstagramClone.Domain/Repositories/IRepository.cs
+++ src/InstagramClone.Domain/Repositories/IRepository.cs
@@ -7,11 +7,39 @@ namespace InstagramClone.Domain.Repositories;
 public interface IRepository<TEntity>
     where TEntity : class {
 
+    // retorno de entidades completas
     Task<TEntity?> FirstOrDefaultAsync(
     Expression<Func<TEntity, bool>> where,
     bool tracking = true,
     CancellationToken cancellationToken = default);
 
-    //Task<IReadLeadTEntity> ToListAsync()
+    Task<IReadOnlyCollection<TEntity>> ToListAsync(
+        Expression<Func<TEntity, bool>>? where = null,
+        bool tracking = false,
+        CancellationToken cancellationToken = default);
+
+    // retorno de projections (dtos)
+    Task<TProjection?> FirstOrDefaultAsync<TProjection>(
+        Expression<Func<TEntity, TProjection>> selector,
+        Expression<Func<TEntity, bool>> where,
+        CancellationToken? cancellationToken = default);
+
+    Task<IReadOnlyCollection<TProjection>> ToListAsync<TProjection>(
+        Expression<Func<TEntity, TProjection>> selector,
+        Expression<Func<TEntity, bool>>? where = null,
+        bool tracking = false,
+        CancellationToken? cancellationToken = default);
+        
+    //outros
+    Task<bool> AnyAsync(
+        Expression<Func<TEntity, bool>> where,
+        CancellationToken cancellationToken = default);
+
+    // alteração
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    void Update(TEntity entity);
+    void UpdateRange(IEnumerable<TEntity> entity);
+    void Remove(TEntity entity);
 
 }
---
+++ src/InstagramClone.Infrasctructure/Repositories/EfRepository.cs
@@ -0,0 +1,59 @@
+﻿using InstagramClone.Domain.Entities;
+using InstagramClone.Domain.Repositories;
+using System.Linq.Expressions;
+
+namespace InstagramClone.Infrasctructure.Repositories;
+
+internal class EfRepository<TEntity>() : IRepository<TEntity>
+        where TEntity : class, IEntity 
+{
+    public Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> where, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> where, bool tracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TProjection?> FirstOrDefaultAsync<TProjection>(Expression<Func<TEntity, TProjection>> selector, Expression<Func<TEntity, bool>> where, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyCollection<TEntity>> ToListAsync(Expression<Func<TEntity, bool>>? where = null, bool tracking = false, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyCollection<TProjection>> ToListAsync<TProjection>(Expression<Func<TEntity, TProjection>> selector, Expression<Func<TEntity, bool>>? where = null, bool tracking = false, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateRange(IEnumerable<TEntity> entity)
+    {
+        throw new NotImplementedException();
+    }
+}
